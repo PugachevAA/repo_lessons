231f14ceace0f0dd5f84d2235829218436e58a9b
Alexey <pugachev.aa@mail.ru>
2021-04-19 22:45:57 +0700

    lesson 5

---
+++ src/main/java/ru/geekbrains/qr_1/level_2/lesson_5/Main.java
@@ -0,0 +1,11 @@
+package ru.geekbrains.qr_1.level_2.lesson_5;
+
+import ru.geekbrains.qr_1.level_2.lesson_5.arrays.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        Arrays.array1Thrd();
+        Arrays.array2Thrds();
+    }
+
+}
